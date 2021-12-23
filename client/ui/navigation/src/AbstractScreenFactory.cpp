5b102d33238c22e93235acde3d6428a273f7e0ec
rinatkh <khairullinrinat.99@gmail.com>
2021-12-23 20:19:31 +0300

    add client

--- client/ui/navigation/src/AbstractScreenFactory.cpp
+++
@@ -1,6 +0,0 @@
-#include "AbstractScreenFactory.h"
-
-AbstractScreenFactory::AbstractScreenFactory(){}
-AbstractScreenFactory::~AbstractScreenFactory(){}
-AbstractFragment* AbstractScreenFactory::createFragment(QString flag){}
-QString AbstractScreenFactory::createStart(){}
--- client/webcam/main.cpp
+++
@@ -1,8 +0,0 @@
-#include "webcam.h"
-
-int main(int argc, char* argv[]){
-    Broadcaster* server = new Broadcaster;
-    server->launchServer();
-    delete server;
-    return 0;
-}
